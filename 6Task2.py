dda54897481edf4b2a0f61a6f7930b274cf8d9c3
Jaisaibaba236 <jay.patel76@dal.ca>
2018-01-23 18:00:41 -0400

    Task2 has been completed

--- 6Task2.py
+++ 6Task2.py
@@ -13,8 +13,8 @@ corner = point()
 #Creating attributes
 direct_instance.height = 200
 direct_instance.width = 100
-corner.x = 3
-corner.y = 4
+corner.x = 0
+corner.y = 0
 
 def find_center(direct_instance,corner):
    dx = corner.x + (direct_instance.width/2)
