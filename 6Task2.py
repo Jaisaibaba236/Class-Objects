d54c4779eee69c4542a8c7ac49906a2b59ceb5ab
Jaisaibaba236 <jay.patel76@dal.ca>
2018-01-23 17:57:59 -0400

    Task2 has been completed

---
+++ 6Task2.py
@@ -0,0 +1,24 @@
+#creating a class object named point
+class point():
+    print('FirstCode')
+
+#creating a class object named rectangle
+class rectangle():
+  'Finding centre point of Rectangle'
+
+#Creating instances
+direct_instance = rectangle()
+corner = point()
+
+#Creating attributes
+direct_instance.height = 200
+direct_instance.width = 100
+corner.x = 3
+corner.y = 4
+
+def find_center(direct_instance,corner):
+   dx = corner.x + (direct_instance.width/2)
+   dy = corner.y + (direct_instance.height/2)
+   print('Center of rectangle is','(',dx,',',dy,')')
+
+find_center(direct_instance,corner)
